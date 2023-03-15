67207da16d3c1c67d4821fe168c1474f104a608d
rsalz47 <ronan.salz@gmail.com>
2023-03-15 13:08:51 -0400

    unified memory workspace

---
+++ src/mem/memory.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "memory.h"
+
+using namespace std;
+
+
+int main() {
+    reg7 = 1231;
+    uint32_t address = 0;
+    write_to_memory(0,reg7);
+    cout << read_from_memory(0);
+    return 0;
+}
