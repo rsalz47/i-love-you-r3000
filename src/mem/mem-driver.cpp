3d7747b94b295ca27456d0a7fae2b586edcf282f
Srinanda Kishore Yallapragada <srinanda.yallapragada@gmail.com>
2023-03-17 12:13:30 -0400

    reworked memory functions to follow clock 'push'. Added mem_access struct that contains the delay_int

--- src/mem/mem-driver.cpp
+++ src/mem/mem-driver.cpp
@@ -1,26 +1,49 @@
-// make another file called "std_includes.h" for std libs like stdio, unistd and iostream etc
+// make another file called "std_includes.h" for std libs like stdio, unistd and
+// iostream etc
 
-#include <stdio.h>
-#include <unistd.h> 
-#include <iostream>
 #include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+
 #include "memory.h"
 #define MEM_DELAY 100
 using namespace std;
 
-int main()
-{
-    int delay1 = MEM_DELAY;
-    int delay2 = MEM_DELAY;
+int main() {
     Memory main_mem;
-    main_mem.store(1, uint32_t(7777), &delay1);
-    main_mem.store(2, uint32_t(7777), &delay1);
-    main_mem.store(3, uint32_t(7777), &delay1);
-    main_mem.store(4, uint32_t(7777), &delay1);
-    main_mem.store(123,uint32_t(7777),&delay1);
-    main_mem.cur_status();
-    cout << main_mem.fetch(4,&delay2) << endl;
-    // cout << main_mem.fetch(2,&delay2) << endl;
-    // main_mem.reset();
-    return 0;
+    volatile int clock;
+
+    mem_access store_req1 = {
+        0,              // sucess
+        uint32_t(777),  // data
+        6,              // addr
+        10              // delay int
+    };
+
+    mem_access fetch_req1 = {
+        0,               // sucess
+        uint32_t(NULL),  // data
+        6,               // addr
+        5                // delay int
+    };
+
+    //TODO switch to while(clock++) when we need to let the clock run to infinity/ EOF
+
+    for (clock = 0; clock < 200; clock++) {
+        // main_mem.store is called in every iteration of the for loop until it
+        // returns 1
+        if (main_mem.store(&store_req1) > 0) {
+            cout << store_req1.sucess << endl;
+            break;  // stops ticking of clock as the access is finished here
+        }
+    }
+    for (;clock < 200; clock++) {
+        // main_mem.fetch is called in every iteration of the for loop until it
+        // returns 1
+        if (main_mem.fetch(&fetch_req1) > 0) {
+            cout << fetch_req1.data << endl;
+            break;  // stops ticking of clock as the access is finished here
+        }
+    }
 }
--- src/mem/memory.h
+++ src/mem/memory.h
@@ -1,61 +1,68 @@
-// words and lines values taken from slide deck 7 example of memory calculations.
+// words and lines values taken from slide deck 7 example of memory
+// calculations.
 #define WORDS_PER_LINE 4
 #define NUM_LINES 32
-class Memory
-{
-public:
-    // spent an hour debugging this. using memory[524288][4] is too large for the code stack. using defaults from slides
+
+struct mem_access {
+    int sucess;
+    uint32_t data;
+    uint32_t addr;
+    int delay_timer;
+};
+
+class Memory {
+   public:
+    // spent an hour debugging this. using memory[524288][4] is too large for
+    // the code stack. using defaults from slides
     uint32_t memory[NUM_LINES][WORDS_PER_LINE];
 
     // return value stored memory
-    uint32_t fetch(uint32_t addr, int *delay_int)
-    {
-        // with this solution, any pipeline that calls fetch, will be waiting for fetch to return and does not need to call fetch every single time as a spin lock
-        while (*delay_int != 0)
-        {
-            (*delay_int) = (*delay_int) - 1;
+    int fetch(mem_access *fetch_req) {
+        if (fetch_req->delay_timer != 0) {
+            fetch_req->delay_timer -= 1;
+            return -1;
         }
         // after waiting the appropriate amount of time,
         // fetch from the memory array at ADDR
-        uint32_t ret_val = this->memory[(addr / WORDS_PER_LINE) % (WORDS_PER_LINE * NUM_LINES)][addr % WORDS_PER_LINE];
-        return ret_val;
+        fetch_req->data = this->memory[(fetch_req->addr / WORDS_PER_LINE) %
+                                       (WORDS_PER_LINE * NUM_LINES)]
+                                      [fetch_req->addr % WORDS_PER_LINE];
+        fetch_req->sucess = 1;
+        return 1;
     }
     // stores data in memory[addr]
-    uint32_t store(uint32_t addr, uint32_t data, int *delay_int)
-    {
-        while (*delay_int != 0)
-        {
-            (*delay_int) = (*delay_int) - 1;
+    int store(mem_access *store_req) {
+        if (store_req->delay_timer != 0) {
+            store_req->delay_timer -= 1;
+            return -1;
         }
         // after waiting the appropriate amount of time,
         // store DATA in the memory array at ADDR
-        this->memory[(addr / WORDS_PER_LINE) % (WORDS_PER_LINE * NUM_LINES)][addr % WORDS_PER_LINE] = data;
+        this->memory[(store_req->addr / WORDS_PER_LINE) %
+                     (WORDS_PER_LINE * NUM_LINES)]
+                    [store_req->addr % WORDS_PER_LINE] = store_req->data;
+        store_req->sucess = 1;
         return 1;
     }
-    // assign all memory locations to NULL (essentially 0s) 
-    void reset()
-    {
-        for (int i = 0; i < NUM_LINES; i++)
-        {
-            for (int j = 0; j < WORDS_PER_LINE; j++)
-            {
-                // uint32_t(NULL) = 0 as it writes is as 32bits of 0 I think
+    // assign all memory locations to NULL (essentially 0s)
+    void reset() {
+        for (int i = 0; i < NUM_LINES; i++) {
+            for (int j = 0; j < WORDS_PER_LINE; j++) {
+                // uint32_t(NULL) = 0 as it writes is as 32bits of 0
                 this->memory[i][j] = uint32_t(NULL);
             }
         }
     }
-    // prints out a formatted memory status in the terminal 
-    void cur_status()
-    {
+    // prints out a formatted memory status in the terminal
+    void cur_status() {
         {
-            for (int i = 0; i < NUM_LINES; i++)
-            {
-                for (int j = 0; j < WORDS_PER_LINE; j++)
-                {
-                    printf("%15d | ",this->memory[i][j]);
+            for (int i = 0; i < NUM_LINES; i++) {
+                for (int j = 0; j < WORDS_PER_LINE; j++) {
+                    printf("%15d | ", this->memory[i][j]);
                 }
-                printf("\n--------------- | --------------- | --------------- | --------------- | \n");
-                
+                printf(
+                    "\n--------------- | --------------- | --------------- | "
+                    "--------------- | \n");
             }
         }
     }
