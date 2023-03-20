12c72424794cb97a4d3cbb99ab81970b8f121296
Jiangyi Qiu <qiujyr@gmail.com>
2023-03-20 00:24:20 -0400

    mem test code fixed

--- test/mem/test_memory.cpp
+++ test/mem/test_memory.cpp
@@ -3,10 +3,6 @@
 
 #define IS_TRUE(x) { if (!(x))  std::cout <<__FUNCTION__ << " failed on line " << __LINE__ << std::endl; }
 
-// address is big
-// correct clock cycle count
-// 
-
 //Test for fetch with 0 delay
 void test_fetch_zero_delay() {    
   Memory mem; //delay 0
@@ -39,14 +35,13 @@ void test_fetch_nonzero_delay() {
   int clock = 0;
   
   for(;;clock++) {
-    data = mem.fetch(3*NUM_LINES+2,0);
+    data = mem.fetch(3*WORDS_PER_LINE+2,0);
     if (data != nullptr) {
       break;
     }
   }
   IS_TRUE(clock == delay);
   IS_TRUE(*data == expected_data);
-  std::cout << *data << std::endl;
 }
 
 
@@ -61,7 +56,7 @@ void test_store_nonzero_delay() {
   int clock = 0;
   
   for (;;clock++) {
-    data = mem.store(3*NUM_LINES+2 ,data_stored, 0);
+    data = mem.store(3*WORDS_PER_LINE+2 ,data_stored, 0);
     if (data != nullptr) {
       break;
     }
@@ -72,16 +67,47 @@ void test_store_nonzero_delay() {
 }
 
 //Test for load and fetch with large addresses
+//The formula for memery access is
+// memory[(addr / WORDS_PER_LINE) % (WORDS_PER_LINE * NUM_LINES)][addr % WORDS_PER_LINE])
+//Want to test the case where (addr / WORDS_PER_LINE) is greater than NUM_LINES
 void test_memory_with_large_addresses() {
+  int delay = 5;
+  uint32_t* data;
+  uint32_t data_stored = 1024;
   
+  Memory mem;
+  int clock = 0;
+  uint32_t addr = (NUM_LINES+1)*WORDS_PER_LINE;
+  
+  //store
+  for (;;clock++) {
+    data = mem.store(addr, data_stored, 0);
+    if (data != nullptr) {
+      break;
+    }
+  }
 
+  IS_TRUE(clock == delay);
+  IS_TRUE(*data == data_stored);
+
+  //load
+  clock = 0;
+  for (;;clock++) {
+    data = mem.fetch(addr, 0);
+    if (data != nullptr) {
+      break;
+    }
+  }
+  IS_TRUE(clock == delay);
+  IS_TRUE(*data == data_stored);
 }
 
 
 
 int main() {
-  //  test_fetch();
-  //  test_load();
+  //  test_fetch_zero_delay();
+  //  test_load_zero_delay();
   test_fetch_nonzero_delay();
   test_store_nonzero_delay();
+  //  test_memory_with_large_addresses();
 }
