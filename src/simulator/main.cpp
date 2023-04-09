8c397398eb9ae6119b6366802d7cad60e6c8203c
Srinanda Kishore Yallapragada <srinanda.yallapragada@gmail.com>
2023-04-09 17:15:23 -0400

    removing pipeline.cpp as it is a file filled only with comments and no actual working code

--- src/simulator/main.cpp
+++ src/simulator/main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
-
+#include <QString>
+#include <QDebug>
 #include <QApplication>
+
+#include "../../src/mem/cache.cpp"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    qInfo("hello");
     return a.exec();
 }
