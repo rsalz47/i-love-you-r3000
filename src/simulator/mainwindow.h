33c4afbaeb1ab3cd120d426e036bc468cfa133a9
Srinanda Kishore Yallapragada <srinanda.yallapragada@gmail.com>
2023-04-09 17:08:08 -0400

    UI reworking

--- src/simulator/mainwindow.h
+++ src/simulator/mainwindow.h
@@ -2,14 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QFile>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
-class RegistersWidget;
-class CodeWidget;
 
 class MainWindow : public QMainWindow
 {
@@ -19,14 +15,7 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_loadAssembButton_clicked();
-
-    void on_quicLoadButton_clicked();
-
 private:
     Ui::MainWindow *ui;
-
-    void loadAssembly(QString filename);
 };
 #endif // MAINWINDOW_H
