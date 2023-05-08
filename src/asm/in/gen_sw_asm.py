fc62d49a31a1c0338aa3bd2c612eb4b27c228451
Jiangyi Qiu <qiujyr@gmail.com>
2023-05-08 00:35:32 -0400

    modified python script comments

--- src/asm/in/gen_sw_asm.py
+++ src/asm/in/gen_sw_asm.py
@@ -1,12 +1,14 @@
 # This program generates the assembly file that stores
-# some specified data in a file to memory with specified # starting address
+# data in a file to memory starting from some address
+# The file and the address are specified by command line arguments
+
 # argv[0]: starting address
-# argv[1]: file that contains the data
+# argv[1]: file that contains the data, with type suffix .dat
 import sys
 
 starting_addr = int(sys.argv[1]);
 data_filename = sys.argv[2];
-# data filename has the format .dat
+# remove .dat and append .ily
 output_filename = data_filename.split(".", 1)[0] + ".ily"
 
 data = []
@@ -26,8 +28,4 @@ for num in data:
     output_file.write("li r1 " + str(num) + "\n")
     output_file.write("sw r1 r0 " + str(starting_addr) + "\n")
     starting_addr += 1
-output_file.close()
-    
-    
-
-    
+output_file.close()    
