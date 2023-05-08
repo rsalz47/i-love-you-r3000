1bb1b6708ea1d98e545eed4dfe56adb47802e458
Jiangyi Qiu <qiujyr@gmail.com>
2023-05-08 19:13:56 -0400

    modified exchange sort script, now it only takes the filename as the cml argument and the starting address is computed by the program

--- src/asm/in/exchange_sort_gen.py
+++ src/asm/in/exchange_sort_gen.py
@@ -1,16 +1,16 @@
 # This program generates the assembly file that sorts the data
 # in the incresing order using exchange sort given a file containing
-# the data and the starting address of the memory to store the data
+# the data
 
-# The file and the address are specified by command line arguments
+# The filename is specified by command line arguments
 
 # argv[0]: starting address
 # argv[1]: file that contains the data, with type suffix .dat
 import sys
 import re
 
-starting_addr = int(sys.argv[1]);
-data_filename = sys.argv[2];
+#starting_addr = int(sys.argv[1]);
+data_filename = sys.argv[1];
 # remove .dat and append .ily
 output_filename = data_filename.split(".", 1)[0] + ".ily"
 
@@ -26,6 +26,12 @@ for line in data_file:
 data_file.close()
 print(data)
 
+size = len(data) - data.count("")
+starting_addr = 1 + 2*size + 16
+
+if starting_addr % 4:
+    starting_addr += (4 - (starting_addr % 4))
+
 output_file = open(output_filename, 'w+')
 
 output_file.write("li r0 0\n")
@@ -34,7 +40,9 @@ for num in data:
     if num != "":
         output_file.write("li r1 " + str(num) + "\n")
         output_file.write("sw r1 r0 " + str(starting_addr) + "\n")
+        size += 1
         starting_addr += 1
+
 output_file.write("li r2 0\n\
 li r3 99\n\
 li r7 100\n\
@@ -42,11 +50,11 @@ li r7 100\n\
 addi r4 r2 1\n\
 .innerloop beq r4 r7 .exitinner\n\
 ")
-output_file.write("lw r5 r4 " + sys.argv[1] + "\n")
-output_file.write("lw r6 r2 " + sys.argv[1] + "\n")
+output_file.write("lw r5 r4 " + str(starting_addr) + "\n")
+output_file.write("lw r6 r2 " + str(starting_addr) + "\n")
 output_file.write("blt r6 r5 .exitif\n")
-output_file.write("sw r5 r2 " + sys.argv[1] + "\n")
-output_file.write("sw r6 r4 " + sys.argv[1] + "\n")
+output_file.write("sw r5 r2 " + str(starting_addr) + "\n")
+output_file.write("sw r6 r4 " + str(starting_addr) + "\n")
 output_file.write("\
 .exitif addi r4 r4 1\n\
 j .innerloop\n\
